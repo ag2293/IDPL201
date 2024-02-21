4b86982ec7fd7cf012dbafbcee9df91caa4dba5f
Ming Zeng <72892319+mz450@users.noreply.github.com>
2024-02-21 10:44:44 +0000

    Update ToFSensorTest.ino

--- IDP Sensor Testing/ToFSensorTest/ToFSensorTest.ino
+++ IDP Sensor Testing/ToFSensorTest/ToFSensorTest.ino
@@ -14,11 +14,28 @@ void setup() {
  //Laser rangefinder begins to work
  sensor.start();
 }
-void loop()
-{
- //Get the distance
- Serial.print("Distance: ");Serial.println(sensor.getDistance());
- //The delay is added to demonstrate the effect, and if you do not add the delay,
- //it will not affect the measurement accuracy
- delay(500);
+
+
+
+void loop() {
+  // Get the distance in meters
+  float distance = sensor.getDistance();
+
+  // Convert distance to centimeters (1 meter = 100 centimeters)
+  float distance_cm = distance * 100;
+
+  // Print the distance to the Serial Monitor
+  Serial.print("Distance: ");
+  Serial.print(distance_cm);
+  Serial.println(" cm");
+
+  // Check if the distance is smaller than 1 cm
+  if (distance_cm < 1.0) {
+    // Command to stop the robot
+    robotStop();
+    Serial.println("Robot stopped due to close obstacle.");
+  }
+
+  // The delay is added to demonstrate the effect, and it will not affect the measurement accuracy
+  delay(500);
 }
